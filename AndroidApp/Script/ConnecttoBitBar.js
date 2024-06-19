6841605cf7a098d8d68ab79925e2062ed45f24a1
NkosisamboGIT <148614830+NkosisamboGIT@users.noreply.github.com>
2024-06-19 08:25:43 +0200

    Changes

--- AndroidApp/Script/ConnecttoBitBar.js
+++ AndroidApp/Script/ConnecttoBitBar.js
@@ -1,15 +1,54 @@
 ﻿function ConnecttoBitBar()
 {
   var capabilities = {
+  "autoGrantPermissions": "true",
+  "appActivity": "com.android.permissioncontroller.permission.ui.GrantPermissionsActivity",
+  "appPackage": "com.google.android.permissioncontroller",
   "platformName": "ANDROID", 
   "bitbar_target": "android", 
   "bitbar_findDevice": "true", 
   "bitbar_multiSessionWait": "60", 
-  "bitbar_device": "Samsung Galaxy S9+ SM-G965F", 
-  "deviceName": "Samsung Galaxy S9+ SM-G965F", 
+  "bitbar_device": "Google Pixel 3a Android 12", 
+  "deviceName": "Google Pixel 3a Android 12", 
   "automationName": "UiAutomator2", 
   "bitbar_app": "232824667"
-};
-var server = "https://eu-mobile-hub.bitbar.com/wd/hub";
-Mobile.ConnectDevice(server, capabilities);
+//    "autoGrantPermissions": "true",
+//    "appActivity": "com.android.permissioncontroller.permission.ui.GrantPermissionsActivity",
+//    "appPackage": "com.google.android.permissioncontroller"
+  };
+  var server = "https://eu-mobile-hub.bitbar.com/wd/hub";
+  Mobile.ConnectDevice(server, capabilities, 600);
+  
+  let device = Aliases.Device;
+  device.processGoogleAndroidAppsNexuslauncher.scrollViewWorkspace.textView.Touch();
+  let processCloudbadgerStandardbank = device.Process_cloudbadger_standardbank;
+  let viewGroup = processCloudbadgerStandardbank.viewGroup;
+  viewGroup.Touch();
+  viewGroup.Touch();
+  viewGroup.Touch();
+  viewGroup.Touch();
+  viewGroup = processCloudbadgerStandardbank.viewGroup2;
+  viewGroup.Touch();
+  viewGroup.Touch();
 }
+
+//
+//function ConnecttoBitBar()
+//{
+// var capabilities = {
+//  "platformName": "ANDROID", 
+//  "bitbar_target": "android", 
+//  "bitbar_findDevice": "true", 
+//  "bitbar_multiSessionWait": "60", 
+//  "bitbar_device": "Samsung Galaxy S9+ SM-G965F", 
+//  "deviceName": "Samsung Galaxy S9+ SM-G965F", 
+//  "automationName": "UiAutomator2", 
+//  "bitbar_app": "232824667",
+//  "autoGrantPermissions": "true",
+//  "appActivity": "com.android.permissioncontroller.permission.ui.GrantPermissionsActivity",
+//  "appPackage": "com.google.android.permissioncontroller"
+//
+//};
+//var server = "https://eu-mobile-hub.bitbar.com/wd/hub";
+//Mobile.ConnectDevice(server, capabilities);
+//}var capabilities = {
--- USSD_CB/Script/LogInUSSD.js
+++ USSD_CB/Script/LogInUSSD.js
@@ -11,6 +11,48 @@
     Sys.Browser().BrowserWindow(0).Maximize();
     var page = Sys.Browser("*").Page("*");
     
-    // Click login button
-    page.Find("contentText", "Create a new Session", 1000).Click();
+    // Click create a new session button
+    Delay(2000);
+    page.NativeWebObject.Find("contentText", "Create a new Session").Click();
+    
+    // select an instance
+    page.FindChildByXPath("//*[@id='instanceId']").Click();
+    Delay(2000);
+    page.formInstanceId.selectInstanceId.ClickItem("PREPROD Eswatini (f08b0)");
+    //page.FindChildByXPath("//*[@id='instanceId']/option[3]").Click();
+   //page.NativeWebObject.Find("ContentText","PREPROD Eswatini (f08b0)").Click();
+     
+    //Enter Mobile Number
+    page.FindChildByXPath("//*[@id='msisdn']").Click();
+    page.FindChildByXPath("//*[@id='msisdn']").SetText(+27730164633);
+     
+    // Enter Network ID
+   page.FindChildByXPath("//*[@id='networkId']").Click();
+   page.FindChildByXPath("//*[@id='networkId']").SetText(1);
+    
+   // Enter IMSI
+   page.FindChildByXPath("//*[@id='imsi']").Click();
+   page.FindChildByXPath("//*[@id='imsi']").SetText(1);
+    
+   //Apply
+   page.FindChildByXPath("//*[@id='newSessionDetailsModal']/div/div/div[2]/form/button[2]").Click();
+   
+   //Submit
+   page.FindChildByXPath("//*[@id='frm_Input']/button").Click();
+   
+   // Enter the password
+   page.FindChildByXPath("//*[@id='inputText']").SetText(55640);
+   
+   //Submit
+   page.FindChildByXPath("//*[@id='frm_Input']/button").Click();
 }
+
+//function Test1()
+//{
+//  Browsers.Item(btChrome).Navigate("https://ussd-uat.unayo.com/");
+//  let browser = Aliases.browser;
+//  browser.BrowserWindow.Maximize();
+//  let page = browser.pageUssdUatUnayoComMsisdn2b27730;
+//  page.textnodeSession.buttonCreateANewSession.ClickButton();
+//  page.formInstanceId.selectInstanceId.ClickItem("PREPROD Eswatini (f08b0)");
+//}
