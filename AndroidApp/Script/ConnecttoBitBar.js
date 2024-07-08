5a53b17532455d3944c43f214a0ef786af13d5b8
NkosisamboGIT <148614830+NkosisamboGIT@users.noreply.github.com>
2024-07-08 14:10:24 +0200

    test

--- AndroidApp/Script/ConnecttoBitBar.js
+++ AndroidApp/Script/ConnecttoBitBar.js
@@ -1,54 +1,22 @@
 ﻿function ConnecttoBitBar()
 {
+
   var capabilities = {
-  "autoGrantPermissions": "true",
-  "appActivity": "com.android.permissioncontroller.permission.ui.GrantPermissionsActivity",
-  "appPackage": "com.google.android.permissioncontroller",
-  "platformName": "ANDROID", 
-  "bitbar_target": "android", 
-  "bitbar_findDevice": "true", 
-  "bitbar_multiSessionWait": "60", 
-  "bitbar_device": "Google Pixel 3a Android 12", 
-  "deviceName": "Google Pixel 3a Android 12", 
-  "automationName": "UiAutomator2", 
-  "bitbar_app": "232824667"
-//    "autoGrantPermissions": "true",
-//    "appActivity": "com.android.permissioncontroller.permission.ui.GrantPermissionsActivity",
-//    "appPackage": "com.google.android.permissioncontroller"
+    "appActivity": "com.android.permissioncontroller.permission.ui.GrantPermissionsActivity", 
+    "appPackage": "com.google.android.permissioncontroller", 
+    "autoGrantPermissions": "true", 
+    "platformName": "ANDROID", 
+    "bitbar_target": "android", 
+    "bitbar_findDevice": "true", 
+    "bitbar_multiSessionWait": "60", 
+    "bitbar_device": "Google Pixel 6 Pro 5G -US", 
+    "deviceName": "Google Pixel 6 Pro 5G -US", 
+    "automationName": "UiAutomator2", 
+    "bitbar_app": "232824667"
   };
   var server = "https://eu-mobile-hub.bitbar.com/wd/hub";
   Mobile.ConnectDevice(server, capabilities, 600);
-  
-  let device = Aliases.Device;
-  device.processGoogleAndroidAppsNexuslauncher.scrollViewWorkspace.textView.Touch();
-  let processCloudbadgerStandardbank = device.Process_cloudbadger_standardbank;
-  let viewGroup = processCloudbadgerStandardbank.viewGroup;
-  viewGroup.Touch();
-  viewGroup.Touch();
-  viewGroup.Touch();
-  viewGroup.Touch();
-  viewGroup = processCloudbadgerStandardbank.viewGroup2;
-  viewGroup.Touch();
-  viewGroup.Touch();
+  Aliases.Device.processGoogleAndroidAppsNexuslauncher.scrollViewWorkspace.textView.Touch();
 }
 
-//
-//function ConnecttoBitBar()
-//{
-// var capabilities = {
-//  "platformName": "ANDROID", 
-//  "bitbar_target": "android", 
-//  "bitbar_findDevice": "true", 
-//  "bitbar_multiSessionWait": "60", 
-//  "bitbar_device": "Samsung Galaxy S9+ SM-G965F", 
-//  "deviceName": "Samsung Galaxy S9+ SM-G965F", 
-//  "automationName": "UiAutomator2", 
-//  "bitbar_app": "232824667",
-//  "autoGrantPermissions": "true",
-//  "appActivity": "com.android.permissioncontroller.permission.ui.GrantPermissionsActivity",
-//  "appPackage": "com.google.android.permissioncontroller"
-//
-//};
-//var server = "https://eu-mobile-hub.bitbar.com/wd/hub";
-//Mobile.ConnectDevice(server, capabilities);
-//}var capabilities = {
+
